3bc031a2f3febf839de07ef7248a8fd8cefecb44
noah the goodra <peterpan0413@live.com>
2016-08-19 21:09:11 -0500

    completed the bookmark version and provided a minified file

--- bookmarklet/src/main.js
+++ bookmarklet/src/main.js
@@ -1,11 +1,39 @@
 /**
  * Created by jfmmeyers on 8/19/16.
  */
-var $ = require('../../libfiles/jquery-3.1.0').noConflict(true);
+$ = require('../../libfiles/jquery-3.1.0.min').noConflict(true);
 var utils = require("./utilfunctions");
-
+window.addEventListener("DOMContentLoaded", stage2,false);
 if($("#ptifrmtgtframe").length)
 {
-    utils.LoadOurselvesIntoIframeButDontExecute("#ptifrmtgtframe");
+
+    utils.LoadOurselves("#ptifrmtgtframe");
     utils.replaceWithIframeContents("#ptifrmtgtframe");
+
+} else if (($(".PAGROUPDIVIDER").length) & (document.title === "My Class Schedule")){
+    window.removeEventListener("DOMContentLoaded",stage2,false)
+    stage2();
 }
+function stage2() {
+    if($(".PSRADIOBUTTON").length != 0)
+    {
+        alert("Error Wrong Page Please go to the printer Friendly Listview Page");
+    }
+    var calandar = require("../../parsefromhtmlfile/src/calandarstuff");
+    var classes = [];
+    extractInfo(classes);
+    utils.downloadfile(calandar(classes).toString(),"cool.ics");
+}
+
+function extractInfo(classarray) {
+    var ClassObj = require("../../parsefromhtmlfile/src/classobjectstuff");
+    $(".PAGROUPDIVIDER").parent().parent().each(function () {
+        var classname = $(this).find(".PAGROUPDIVIDER").text();
+        var parts = $(this).find(".PSLEVEL3GRIDROW").find(".PSEDITBOX_DISPONLY,.PSHYPERLINKDISABLED,.PSLONGEDITBOX").map(function () {
+            return $(this).text();
+        }).get();
+
+        classarray.push(new ClassObj(classname, parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6]));
+    });
+}
+
--- bookmarklet/src/utilfunctions.js
+++ bookmarklet/src/utilfunctions.js
@@ -2,25 +2,34 @@
  * Created by jfmmeyers on 8/19/16.
  */
 //replace the whole document with the contents of the iframe
+
 function replaceWithIframeContents(IframeID) {
-    var iframesrc = jQuery(IframeID).contents().find("html")[0].outerHTML;
+    var iframesrc = $(IframeID).contents().find("html")[0].outerHTML;
     var maindoc = window.document.open("html/text");
-    maindoc.write(iframesrc.toString(),"replace");
+    maindoc.write(iframesrc.toString());
     maindoc.close();
 }
 function runclickEvent(elementid)
 {
     $(elementid).trigger("click");
 }
-function LoadOurselvesIntoIframeButDontExecute(iframeId)
+function LoadOurselves(iframeId)
 {
-        jQuery.get("insert production url here", function(data){
-        var scripttaggy = $("<script>").prop('type', "text/javascript").text("").prop("id","magicspot");
-        jQuery(iframeId).contents().find("head").append(scripttaggy);
-        jQuery(iframeId).contents().find("#magicspot").text(data);
-    },"text");
+    var scriptlink = document.createElement("script");
+
+    scriptlink.src = "https://abc03c76ca6e00838ba5d0aad43921446853c64e.googledrive.com/host/0B4YPWGzx_WjeTXhCWWhFd3dWdEk/bookie.js";
+    $(iframeId).contents()[0].body.appendChild(scriptlink);
+    var checkblock = document.createElement("div");
+    checkblock.id = "loaded";
+    $(iframeId).contents()[0].body.appendChild(checkblock);
 }
 
+function downloadfile(data,filename)
+{
+    var download = require("../../node_modules/downloadjs/download.min");
+    download(data,filename,"text/calendar");
+}
 
 exports.replaceWithIframeContents = replaceWithIframeContents;
-exports.LoadOurselvesIntoIframeButDontExecute = LoadOurselvesIntoIframeButDontExecute;
+exports.LoadOurselves = LoadOurselves;
+exports.downloadfile = downloadfile;
