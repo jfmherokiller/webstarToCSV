94835d3b256d50542eff6a894556baab222014de
noah the goodra <peterpan0413@live.com>
2017-01-18 17:21:07 -0600

    fixed the algorithm this time i hope

--- shared/src/SharedUilityFunctions.js
+++ shared/src/SharedUilityFunctions.js
@@ -1,25 +1,33 @@
-/**
- * Created by jfmmeyers on 1/18/17.
- */
+function GenerateMissingSpots(parts, previousclassparts) {
+    if (parts.length < previousclassparts.length) {
+        var data = [];
+        var length = previousclassparts.length - parts.length; // user defined length
+        for (var i = 0; i < length; i++) {
+            data.push('\xa0');
+        }
+        parts = data.concat(parts);
+    }
+    for(var partsindex = 0; partsindex < parts.length; partsindex++ )
+    {
+        if(parts[partsindex] == '\xa0')
+        {
+            parts[partsindex] = previousclassparts[partsindex]
+        }
+    }
+    return parts;
+}
 function extractInfo(classarray,$) {
     var ClassObj = require("../../parsefromhtmlfile/src/classobjectstuff");
     $('[id^="win0divDERIVED_REGFRM1_DESCR20"]').each(function () {
         var classname = $(this).find(".PAGROUPDIVIDER").text();
-        var previousclassparts = null;
+        var previousclassparts = "";
         $(this).find('[id^="trCLASS_MTG_VW\$"]').each(function () {
             var parts = $(this).find(".PSEDITBOX_DISPONLY,.PSHYPERLINKDISABLED,.PSLONGEDITBOX").map(function () {
                 return $(this).text();
             }).get();
-            for(var partsindex = 0; partsindex < parts.length; partsindex++ )
-            {
-                if(parts[partsindex] == '\xa0')
-                {
-                    parts[partsindex] = previousclassparts[partsindex]
-                }
-            }
+            parts = GenerateMissingSpots(parts, previousclassparts);
             previousclassparts = parts;
             classarray.push(new ClassObj(classname, parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6]));
-
         });
     });
 }
